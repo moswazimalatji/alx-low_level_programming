15b567cd6ae1d7b996e7587f103853779333559d
moswazimalatji <moswazimalatji@gmail.com>
2022-04-08 14:58:50 -0700

    betty styling

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,9 @@ int main(void)
 
 	int n;
 	int last;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	
 	if (last <  6 && last != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
@@ -28,9 +26,7 @@ int main(void)
 	} else if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	
 	}
 	 return (0);
-
 }
 
