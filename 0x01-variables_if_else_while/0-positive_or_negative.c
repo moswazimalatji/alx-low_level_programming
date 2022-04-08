74c107e58f84e1011272ab7764f75b7f8040e780
moswazimalatji <moswazimalatji@gmail.com>
2022-04-08 15:30:17 -0700

    added betty styling

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/* main function is used to check if a number
- *  is positive or negative or zero */
+/** main function is used to check if a number
+ *  is positive or negative or zero 
+*/
 int main(void)
 {
 	int n;
@@ -13,13 +14,13 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive\n" ,n);
+		printf("%d is positive\n", n);
 	} else if (n == 0)
 	{
-		printf("%d is zero\n" ,n);
+		printf("%d is zero\n", n);
 	} else
 	{
-		printf("%d is negative\n" ,n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
