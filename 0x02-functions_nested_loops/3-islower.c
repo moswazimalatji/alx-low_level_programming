688059ad451ea2fab2e134842c9d71866e211c69
moswazimalatji <moswazimalatji@gmail.com>
2022-04-12 17:58:57 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _islower - check lowercase
+ *
+ * return: 1 if is lower else 0.
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
