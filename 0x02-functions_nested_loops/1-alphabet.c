e98a929a1584da2f7c9076ba02370a73397ed627
moswazimalatji <moswazimalatji@gmail.com>
2022-04-12 17:19:28 -0700

    print alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
- int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int i;
