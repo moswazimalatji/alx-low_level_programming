1bf6cef769aa95557726f1497f3fa479df27e95a
moswazimalatji <moswazimalatji@gmail.com>
2022-04-12 18:06:18 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
 /**
  * _islower - check lowercase
  *
+ * c - input character
+ *
  * return: 1 if is lower else 0.
  */
 int _islower(int c)
@@ -11,8 +13,6 @@ int _islower(int c)
 	if (islower(c))
 	{
 		return (1);
-	} else
-	{
-		return (0);
 	}
+	return (0);
 }
