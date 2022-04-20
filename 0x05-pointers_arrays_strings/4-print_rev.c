13cf83454f9ea044249d65147453d95eaa14569e
moswazimalatji <moswazimalatji@gmail.com>
2022-04-20 14:06:32 -0700

    added putchar

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,9 +15,14 @@ void print_rev(char *s)
 	for (i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[len - i -1];
+		s[i] = s[len - i - 1];
 		s[len - i - 1] = temp;
 	}
 	
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
 }
 
