fe66d37235356a796a4062a8524c8f4a7481998d
moswazimalatji <moswazimalatji@gmail.com>
2022-04-20 16:57:51 -0700

    updates

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,11 @@
  */
 void puts2(char *str)
 {
-	int i, leng;
+	int i, len;
 
-	leng = strlen(str);
+	len = strlen(str);
 
-	for (i = 0; i < leng; i += 2)
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
