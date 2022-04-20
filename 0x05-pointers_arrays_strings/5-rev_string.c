64fc6150516a7ecd584b5f283a886bcbf7887dd6
moswazimalatji <moswazimalatji@gmail.com>
2022-04-20 16:02:03 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,14 @@
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	int i, len, temp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
