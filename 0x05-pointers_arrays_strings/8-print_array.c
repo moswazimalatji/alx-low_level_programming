b2799624fbe9b79486f6c461c98c9f3ac6d961c9
moswazimalatji <moswazimalatji@gmail.com>
2022-04-20 18:37:12 -0700

    added sizeof

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,11 +16,11 @@ void print_array(int *a, int n)
 	{
 		if ( i != n - 1)
 		{
-			_putchar(a[i]);
+			_putchar(a[i] + '0');
 			_putchar(',');
 		} else
 		{
-			_putchar(a[i]);
+			_putchar(a[i] + '0');
 			_putchar('\n');
 		}
 	}
