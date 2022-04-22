ddb967287b36578af56bff750cee9a518a673730
moswazimalatji <moswazimalatji@gmail.com>
2022-04-21 18:36:58 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,20 @@
  */
 int _atoi(char *s)
 {
-	int num, i;
+	int num, i, sign;
 
 	i = 0;
 	num = 0;
+	sign = 1;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
 
 	for (i = 0; s[i] != '\0'; i++)
 		num = num * 10 + s[i] - '0';
 
-	return (num);
+	return (num * sign);
 }
