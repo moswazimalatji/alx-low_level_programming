b648551a4896f721af199785137b9e32fe1808cf
Moswazi Petrus Malatji <42552218+moswazimalatji@users.noreply.github.com>
2022-07-06 02:42:19 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,13 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
 
-
-
-	return (n % 10);
-
-
+	last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
