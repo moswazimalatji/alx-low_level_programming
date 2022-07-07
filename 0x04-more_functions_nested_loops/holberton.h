463051d0e56a83339a6b8bb059bbbec944a824e6
Moswazi Petrus Malatji <42552218+moswazimalatji@users.noreply.github.com>
2022-07-08 01:21:02 +0200

    Update holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,11 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
+ * Header file containing prototypes for all functions
+ * used in the 0x04-more_functions_nested_loops directory.
  */
 
 int _putchar(char c);
